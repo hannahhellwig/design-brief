97dd6e33825a9e7778512225b860d3ee929c81ab
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-09-27 16:20:21 +0200

    added writer box

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -38,18 +38,68 @@ class App extends React.Component {
                   <div className="box">
                     <div className="boxImage"><img src ="/images/medium-image-puff-1.jpeg"/></div>
                     <div className="textBox">
-                      <h2>The Boy Who Tamed the Sea</h2>
-                      <p>At 5:30 am, on any morning I can handle it, I&apos;ll slip out of the dark mouth of my covers and creep trough the silver lig..</p>
+                      <div>
+                        <h2>The Boy Who Tamed the Sea</h2>
+                        <p>At 5:30 am, on any morning I can handle it, I&apos;ll slip out of the dark mouth of my covers and creep trough the silver lig..</p>
+                      </div>
+                        <div className="writerBox">
+                          <div className="writerImage"><img src ="/images/writer-1.png"/></div>
+                          <div>
+                            <p><strong>Carl Moore</strong></p>
+                            <p>Oct 27 8 min read</p>
+                          </div>
+                        </div>
                     </div>
                   </div>
                   <div className="box">
                     <div className="boxImage"><img src ="/images/medium-image-puff-2.png"/></div>
                     <div className="textBox">
-                      <h2>The Role of Human Emotions in the Future of Transport</h2>
-                      <p>Getting from A to B is about to feel very different</p>
+                      <div>
+                        <h2>The Role of Human Emotions in the Future of Transport</h2>
+                        <p>Getting from A to B is about to feel very different</p>
+                      </div>
+                      <div className="writerBox">
+                        <div className="writerImage"><img src ="/images/writer-2.jpeg"/></div>
+                        <div>
+                          <p><strong>Ben Bland</strong></p>
+                          <p>Nov 1 7 min read</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                  <div className="box">
+                    <div className="boxImage"><img src ="/images/medium-image-puff-4.jpeg"/></div>
+                    <div className="textBox">
+                      <div>
+                        <h2>The Burning Season</h2>
+                        <p>You cannot save everyone when the fires come to Nothern California</p>
+                      </div>
+                      <div className="writerBox">
+                        <div className="writerImage"><img src ="/images/writer-4.jpeg"/></div>
+                        <div>
+                          <p><strong>Lindsey Smith</strong></p>
+                          <p>Nov 9 31 min read</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="box">
+                    <div className="boxImage"><img src ="/images/medium-image-puff-3.png"/></div>
+                    <div className="textBox">
+                      <div>
+                        <h2>The March of the Losers</h2>
+                        <p>Around the world, the victims are becoming the abusers, and Donald Trump is their king</p>
+                      </div>
+                      <div className="writerBox">
+                        <div className="writerImage"><img src ="/images/writer-3.jpeg"/></div>
+                        <div>
+                          <p><strong>Henry Wismayer</strong></p>
+                          <p>Nov 10 8 min read</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
           </div>
       </div>
     )
