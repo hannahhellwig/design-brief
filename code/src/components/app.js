610010d159beee8acde89192a74316ab5e86b63e
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-10-16 10:51:53 +0200

    cleaned code with eslint and fixed media queries

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -7,7 +7,7 @@ class App extends React.Component {
       <div className="App">
         <div className="headerBoarder">
           <div className="header">
-            <a href=""><strong>Home</strong></a>
+            <a href="" className="homeLink"><strong>Home</strong></a>
             <a href="">Technology</a>
             <a href="">Creativity</a>
             <a href="">Entrepreneurship</a>
@@ -19,88 +19,110 @@ class App extends React.Component {
             <a href="">More</a>
             <button className="getStartedHeaderButton">Get started</button>
           </div>
-         </div>
-         <div className="container">
+        </div>
+        <div className="container">
           <div className="hero">
             <div className="leftHero">
               <h1>Interesting ideas that set your mind in motion. </h1>
-              <p> Hear directly from the people who know it best. From tech to politics to creativity and more - whatever your interest, we&apos;ve got you covered.</p>
+              <p> Hear directly from the people who know it best. From tech to
+              politics to creativity and more - whatever your interest, we&apos;ve got you covered.
+              </p>
               <div className="buttons">
                 <button className="getStartedButton">Get started</button>
                 <button className="learnMoreButton">Learn more</button>
               </div>
             </div>
             <div className="rightHero">
-              <img src ="/images/medium-image.png"/>
+              <img src="/images/medium-image.png" alt="" />
             </div>
           </div>
-              <div className="grid">
-                  <div className="box">
-                    <div className="boxImage"><img src ="/images/medium-image-puff-1.jpeg"/></div>
-                    <div className="textBox">
-                      <div>
-                        <h2>The Boy Who Tamed the Sea</h2>
-                        <p>At 5:30 am, on any morning I can handle it, I&apos;ll slip out of the dark mouth of my covers and creep trough the silver lig..</p>
-                      </div>
-                        <div className="writerBox">
-                          <div className="writerImage"><img src ="/images/writer-1.png"/></div>
-                          <div>
-                            <p><strong>Carl Moore</strong></p>
-                            <p>Oct 27 8 min read</p>
-                          </div>
-                        </div>
-                    </div>
-                  </div>
-                  <div className="box">
-                    <div className="boxImage"><img src ="/images/medium-image-puff-2.png"/></div>
-                    <div className="textBox">
-                      <div>
-                        <h2>The Role of Human Emotions in the Future of Transport</h2>
-                        <p>Getting from A to B is about to feel very different</p>
-                      </div>
-                      <div className="writerBox">
-                        <div className="writerImage"><img src ="/images/writer-2.jpeg"/></div>
-                        <div>
-                          <p><strong>Ben Bland</strong></p>
-                          <p>Nov 1 7 min read</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="box">
-                    <div className="boxImage"><img src ="/images/medium-image-puff-4.jpeg"/></div>
-                    <div className="textBox">
-                      <div>
-                        <h2>The Burning Season</h2>
-                        <p>You cannot save everyone when the fires come to Nothern California</p>
-                      </div>
-                      <div className="writerBox">
-                        <div className="writerImage"><img src ="/images/writer-4.jpeg"/></div>
-                        <div>
-                          <p><strong>Lindsey Smith</strong></p>
-                          <p>Nov 9 31 min read</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="box">
-                    <div className="boxImage"><img src ="/images/medium-image-puff-3.png"/></div>
-                    <div className="textBox">
-                      <div>
-                        <h2>The March of the Losers</h2>
-                        <p>Around the world, the victims are becoming the abusers, and Donald Trump is their king</p>
-                      </div>
-                      <div className="writerBox">
-                        <div className="writerImage"><img src ="/images/writer-3.jpeg"/></div>
-                        <div>
-                          <p><strong>Henry Wismayer</strong></p>
-                          <p>Nov 10 8 min read</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+          <div className="grid">
+            <div className="articleBox">
+              <div className="boxImage">
+                <img src="/images/medium-image-puff-1.jpeg" alt="" />
               </div>
+              <div className="textBox">
+                <div>
+                  <h2>The Boy Who Tamed the Sea</h2>
+                  <p>At 5:30 am, on any morning I can handle it, I&apos;ll slip out of
+                  the dark mouth of my covers and creep trough the silver lig..
+                  </p>
+                </div>
+                <div className="writerBox">
+                  <div className="writerImage">
+                    <img src="/images/writer-1.png" alt="" />
+                  </div>
+                  <div>
+                    <p><strong>Carl Moore</strong></p>
+                    <p>Oct 27 &ensp; 8 min read</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="articleBox">
+              <div className="boxImage">
+                <img src="/images/medium-image-puff-2.png" alt="" />
+              </div>
+              <div className="textBox">
+                <div>
+                  <h2>The Role of Human Emotions in the Future of Transport</h2>
+                  <p>Getting from A to B is about to feel very different</p>
+                </div>
+                <div className="writerBox">
+                  <div className="writerImage">
+                    <img src="/images/writer-2.jpeg" alt="" />
+                  </div>
+                  <div>
+                    <p><strong>Ben Bland</strong></p>
+                    <p>Nov 1 &ensp; 7 min read</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="articleBox">
+              <div className="boxImage">
+                <img src="/images/medium-image-puff-4.jpeg" alt="" />
+              </div>
+              <div className="textBox">
+                <div>
+                  <h2>The March of the Losers</h2>
+                  <p>Around the world, the victims are becoming
+                   the abusers, and Donald Trump is their king
+                  </p>
+                </div>
+                <div className="writerBox">
+                  <div className="writerImage">
+                    <img src="/images/writer-3.jpeg" alt="" />
+                  </div>
+                  <div>
+                    <p><strong>Henry Wismayer</strong></p>
+                    <p>Nov 10 &ensp; 8 min read</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="articleBox">
+              <div className="boxImage">
+                <img src="/images/medium-image-puff-3.png" alt="" />
+              </div>
+              <div className="textBox">
+                <div>
+                  <h2>The Burning Season</h2>
+                  <p>You cannot save everyone when the fires come to Nothern California</p>
+                </div>
+                <div className="writerBox">
+                  <div className="writerImage">
+                    <img src="/images/writer-4.jpeg" alt="" />
+                  </div>
+                  <div>
+                    <p><strong>Lindsey Smith</strong></p>
+                    <p>Nov 9 &ensp; 31 min read</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
     )
   }
